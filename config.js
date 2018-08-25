6caa6871d0cc5bea94517c62152e6de3d7270385
Lovingly <42682205+lovinglyy@users.noreply.github.com>
2018-08-25 11:34:43 -0300

    config.js schema

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {};
+config.botOwner = 'bot owner id';
+config.token = 'bot token';
+config.defaultPrefix = '--';
+config.youtubeAPI = 'youtube v3 api key';
+
+Object.freeze(config);
+
+module.exports = config;
