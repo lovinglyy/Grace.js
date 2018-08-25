205b895622cad21404c8d5ad8410671643bbd3b8
Lovingly <psychonaut004@gmail.com>
2018-08-25 11:43:54 -0300

    ident fix

--- components/commands/music/playlistAdd.js
+++ components/commands/music/playlistAdd.js
@@ -11,13 +11,12 @@ module.exports = {
 	* by a blank space
 	*/
 	cmd: async function( msg, argSeparator, redisClient, youtubeAPI  ) {
-    if ( !redisClient || !youtubeAPI ) return;
-
+		if ( !redisClient || !youtubeAPI ) return;
 		const msgEmbed = msg.embeds[0];
 
 		let songId;
 		let songTitle;
-    if ( msg.content.indexOf( 'youtube.com/watch?v=' ) === -1 ) {
+		if ( msg.content.indexOf( 'youtube.com/watch?v=' ) === -1 ) {
 			const singleArgument = msg.content.substring(argSeparator);
 			if (!singleArgument) return msg.reply( 'please tell me a valid youtube link! *grrr*' );
 
@@ -29,16 +28,16 @@ module.exports = {
 			} else {
 				return msg.reply( 'no results found :p' );
 			}
-				songId = song.items[0].id.videoId;
+			songId = song.items[0].id.videoId;
 		} else if ( msgEmbed.url.indexOf ( 'https://www.youtube.com/watch?v=' ) === 0 ) {
-				songId = msgEmbed.url.substring( 32 );
-				songTitle = msgEmbed.title;
+			songId = msgEmbed.url.substring( 32 );
+			songTitle = msgEmbed.title;
 		} else {
-				return msg.reply( 'please tell me a valid youtube link! *grrr*' );
+			return msg.reply( 'please tell me a valid youtube link! *grrr*' );
 		}
 
-    const hgetAsync = promisify( redisClient.hget ).bind( redisClient );
-    let userPlaylist = await hgetAsync( msg.author.id, "userPlaylist" );
+		const hgetAsync = promisify( redisClient.hget ).bind( redisClient );
+		let userPlaylist = await hgetAsync( msg.author.id, "userPlaylist" );
 
 		if ( userPlaylist.indexOf( songId ) !== -1 ) return msg.reply( 'this song is already in your playlist! :p' );
 		if ( !userPlaylist ) userPlaylist = '';
@@ -51,13 +50,13 @@ module.exports = {
 			}
 			if ( songCount > 14 ) return msg.reply( 'you reached the maximum amount of songs in your playlist, please remove some or clear it :3' );
 		}
-    redisClient.hset( msg.author.id, 'userPlaylist', userPlaylist + ' ' + songId );
+		redisClient.hset( msg.author.id, 'userPlaylist', userPlaylist + ' ' + songId );
 		const songEmbed = { "title": songTitle, "color": 11529967,
-			"url": `https://www.youtube.com/watch?v=${songId}`,
-			"thumbnail": {
-				"url": `https://img.youtube.com/vi/${songId}/hqdefault.jpg`
-    	}
+		"url": `https://www.youtube.com/watch?v=${songId}`,
+		"thumbnail": {
+			"url": `https://img.youtube.com/vi/${songId}/hqdefault.jpg`
 		}
-		msg.channel.send( 'Song added to your playlist!', {embed: songEmbed} );
-  }
+	}
+	msg.channel.send( 'Song added to your playlist!', {embed: songEmbed} );
+}
 };
