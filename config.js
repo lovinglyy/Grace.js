0bc818c30eb80c2573e6eee5098c4ce4cc479586
Lovingly <42682205+lovinglyy@users.noreply.github.com>
2018-08-30 15:59:40 -0300

    Grrrr

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {};
+config.botOwner = 'bot owner id';
+config.token = 'bot token';
+config.defaultPrefix = '--';
+config.youtubeAPI = 'youtube v3 api key, https://developers.google.com/youtube/v3/';
+config.pubgAPI = 'your pubg api key, https://developer.playbattlegrounds.com/';
+  
+Object.freeze(config);
+module.exports = config;
