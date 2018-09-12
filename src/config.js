6abb47a80a16bb9ab5e7c8ea1b88ec006483ebe5
Lovingly <psychonaut004@gmail.com>
2018-09-12 16:29:29 -0300

    Enable new commands

---
+++ src/config.js
@@ -0,0 +1,13 @@
+const config = {};
+config.botOwner = 'bot owner id';
+config.token = 'bot token';
+config.defaultPrefix = '--';
+config.youtubeAPI = 'youtube v3 api key, https://developers.google.com/youtube/v3/';
+config.pubgAPI = 'your pubg api key, https://developer.playbattlegrounds.com/';
+config.enabledCommands = ['DAILY', 'BANK', 'ROULETTE', 'IDC', 'POKE', 'YUMMY', 'POSITIVE', 'HUG', 'ANGRY',
+  'CHARM', 'EW', 'PLAY', 'QUEUE', 'PLAYLISTADD', 'PLAYLIST', 'PLAYLISTCLEAR', 'PLAYLISTREMOVE', 'PUBGSTATS',
+  'DP', 'ASK', 'PURGE', 'BAN', 'REMOVEROLES', 'RANK', 'SETWELCOME', 'ROLEBEAUTIFY', 'STARBOARD', 'LEADERBOARD', 'GUILDINFO', 'CAT', 'KICK', 'BOTMESSAGE'];
+
+Object.freeze(config);
+
+module.exports = config;
