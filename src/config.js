f3dfd12159ef70e55d6eab3034a15f603bb15c51
Mashiro Shiina <33735917+NarutoInstinct@users.noreply.github.com>
2018-10-04 11:24:23 +0200

    Updated config.js to default template
    I regenned the keys xd

--- src/config.js
+++ src/config.js
@@ -1,13 +1,14 @@
 const config = {};
-config.botOwner = '174578160334012416';
-config.token = 'MzkxNzQxNDc0NDEwMjAxMDg4.Doz8_Q.Y_W2WD2AhRc6YoYBSO2sMxeyJlE';
+config.botOwner = 'bot owner id';
+config.token = 'bot token';
 config.defaultPrefix = '--';
-config.youtubeAPI = 'AIzaSyDFexcgEcSbtCof16qRoeiiXz1z1CrHuA4';
-config.pubgAPI = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI1MDJiNzlkMC1hM2FmLTAxMzYtY2E2YS0wN2NiYmEyMzM3ZDEiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTM3OTYyMjEwLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6Ii03ZjJkOTY5Ny0xMzM4LTQzMDgtODUwZC0zNDhjMzNkZmY5YzAifQ.RtRRNVHcVXEzPxViTEOgM9TzYvk05THEl5mqx8mX9B8';
+config.youtubeAPI = 'youtube v3 api key, https://developers.google.com/youtube/v3/';
+config.pubgAPI = 'your pubg api key, https://developer.playbattlegrounds.com/';
 config.enabledCommands = ['DAILY', 'BANK', 'ROULETTE', 'IDC', 'POKE', 'YUMMY', 'POSITIVE', 'HUG', 'ANGRY',
   'CHARM', 'EW', 'PLAY', 'QUEUE', 'PLAYLISTADD', 'PLAYLIST', 'PLAYLISTCLEAR', 'PLAYLISTREMOVE', 'PUBGSTATS',
-  'DP', 'ASK', 'PURGE', 'BAN', 'REMOVEROLES', 'RANK', 'SETWELCOME', 'ROLEBEAUTIFY', 'DEFAULTROLE', 'RANKREWARD',
-  'REWARDLIST', 'STARBOARD', 'LEADERBOARD', 'SELFASSIGN', 'INFO', 'SELF', 'CAT', 'DOG', 'BUNNY', 'KICK', 'BOTMESSAGE', 'SETXP'];
+  'DP', 'HELP', 'ASK', 'PURGE', 'BAN', 'REMOVEROLES', 'RANK', 'SETWELCOME', 'ROLEBEAUTIFY', 'DEFAULTROLE', 'RANKREWARD',
+  'REWARDLIST', 'STARBOARD', 'LEADERBOARD', 'SELFASSIGN', 'GUILDINFO', 'SELF', 'CAT', 'DOG', 'BUNNY', 'KICK',
+  'BOTMESSAGE', 'SETXP'];
 
 Object.freeze(config);
 
